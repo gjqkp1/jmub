ff666275a2a11d4e610d977c8163b0bc0817ee62
gjqkp1 <130083675+gjqkp1@users.noreply.github.com>
2023-04-16 22:10:11 +0000

    Add files via upload

---
+++ jmub/Config/roza_config.py
@@ -0,0 +1,9 @@
+# config values will be loaded from here
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    from config import Config  # noqa
+elif os.path.exists("config.py"):
+    from config import Development as Config  # noqa
